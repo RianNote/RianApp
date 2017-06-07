d2d926252e3ff3f3c32cd2ebed30978767459c20
Wuyialex <cci45@naver.com>
2017-06-07 23:30:23 +0900

    makeGraphQLSchemaAndResolver

--- graphqlServer/qlSchema/schema.js
+++ graphqlServer/qlSchema/schema.js
@@ -1,17 +1,14 @@
-import {
-  makeExecutableSchema,
-  addMockFunctionsToSchema,
-} from 'graphql-tools';
-import NoteSchema from './Note/NoteSchema'
-import TagSchema from './Tag/TagSchema'
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+import NoteSchema from './Note/NoteSchema';
+import TagSchema from './Tag/TagSchema';
 import { resolvers } from '../resolvers/resolvers.js';
 
 const RootQuery = `
   type Query {
-     noteTimeline(userId: ID!): [Note]
      getTagList(userId: ID!): [Tag]
+     getNoteList(tag: [String]): NoteHead
   }
-`
+`;
 
 const SchemaDefinition = `
   schema {
@@ -19,8 +16,8 @@ const SchemaDefinition = `
   }
 `;
 
-const schema = makeExecutableSchema({ 
-  typeDefs: [SchemaDefinition, RootQuery, NoteSchema, TagSchema], 
-  resolvers 
+const schema = makeExecutableSchema({
+  typeDefs: [SchemaDefinition, RootQuery, NoteSchema, TagSchema],
+  resolvers,
 });
-export { schema }
+export { schema };
--- graphqlServer/resolvers/resolvers.js
+++ graphqlServer/resolvers/resolvers.js
@@ -1,38 +1,75 @@
 import { pubsub } from '../pubsub/pubsub.js';
 import Note from 'database/models/note.model.js';
 import Tag from 'database/models/tag.model.js';
+import MockData from 'MockData/MockNoteData.js';
 export const resolvers = {
-
   Query: {
-    noteTimeline(obj, args, context){
-      console.log('obj, args, context', obj, args, context)
-      return Note.find({ userid: args.userId })
-             .select('_id created_at final_modified_at')
-             .exec((err, note) => {
-                return note
-             })
+    getTagList(obj, args, context) {
+      // console.log('tagList', obj, args, context);
+      return Tag.find({ userid: args.userId })
+        .select('name')
+        .exec((err, tag) => {
+          console.log('tag', tag);
+          return tag;
+        });
     },
 
-    getTagList(obj, args, context){
-      console.log('tagList', obj, args, context)
-      return Tag.find({ userid: args.userId})
-             .select('name')
-             .exec((err, tag) => {
-                console.log('tag', tag)
-                return tag
-             }) 
-    }
-  },
-
-  Note: {
-    preview(obj, args, context){
-      return Note.findById(obj._id)
-             .select('title snippet')
-             .exec((err, note) => {
-                return note
-             })
+    getNoteList(obj, args, context) {
+      // 태그 상관없이 전체 노트 쿼리
+      console.log('getNoteList args', args);
+      if (!args.tag) {
+        return {
+          tag: [''],
+          totalCount: MockData.length,
+        };
+      }
+      // 특정 태그 찝는 쿼리
+      let count = 0;
+      MockData.forEach((data) => {
+        if (data.tag === args.tag) {
+          count++;
+        }
+      });
+      return {
+        tag: [''],
+        totalCount: count,
+      };
     },
   },
 
+  NoteHead: {
+    notes(ojb, args, context) {
+      const startPoint = MockData.find(data => data._id.$oid === args.after);
+      let result = [];
+      const startIndex = MockData.indexOf(startPoint) + 1;
+      const endIndex = startIndex + (args.limit ? args.limit : MockData.length);
+      for (let i = startIndex; i < endIndex; i++) {
+        result.push(MockData[i]);
+      }
+      if (result.length === 0) {
+        return [
+          {
+            _id: '12341231111a',
+            title: '',
+            preview: '',
+            tag: '',
+            image: '',
+            publish: false,
+            star: 30,
+            created_at: '2013/03/29',
+            final_modified_at: '2015/03/29',
+          },
+        ];
+      }
+      return result;
+    },
+
+    pageInfo(obj, args, context) {
+      console.log('pageInfo', obj);
+      return {
+        endCursor: '12341231111a',
+        isLastPage: false,
+      };
+    },
+  },
 };
-
